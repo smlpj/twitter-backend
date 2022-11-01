2fab93eb9040205f82d9c818ad1befb07559f9da
smlpj <slopeza@uninorte.edu.co>
2022-11-01 14:40:29 -0500

    Mongoose and nodemon added to project

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ mongoose
   .connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
   })
   .then(() => console.log("Conectado a MongoDB."))
   .catch((err) => console.log("Error al conectar a MongoDB.", err));
+
+const userSchema = mongoose.Schema({
+  name: String,
+  age: Number,
+  tweets: Array,
+  followers: Array,
+  following: Array,
+});
+
+const UserModel = mongoose.model("users", userSchema);
+
+//Show
+
+const show = async () => {
+  const users = await UserModel.find();
+  console.log(users);
+};
+
+show();
