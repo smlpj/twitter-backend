75b22401b1bc0d1b3f9acf8a6cc00e2edd5155f7
smlpj <slopeza@uninorte.edu.co>
2022-11-01 17:19:08 -0500

    Tweet CRUD done

--- routes/tweet.js
+++ routes/tweet.js
@@ -1,25 +1,59 @@
 const express = require("express");
-const userSchema = require("../models/tweets");
+const userSchema = require("../models/users");
+const tweetSchema = require("../models/tweets");
 
 const router = express.Router();
 
-//Create tweet
+//Create tweet and add it to user
 router.post("/tweets", (req, res) => {
-  const tweet = userSchema(req.body);
+  const tweet = tweetSchema({
+    user: req.body.user,
+    tweet: req.body.tweet,
+  });
   tweet
     .save()
     .then((data) => {
-      res.json(data);
+      userSchema
+        .findByIdAndUpdate(req.body.user, {
+          $push: { tweets: data._id },
+        })
+        .then((data) => {
+          res.json(data);
+        })
+        .catch((err) => {
+          res.json({ message: err });
+        });
     })
     .catch((err) => {
       res.json({ message: err });
     });
 });
 
-//Get all tweets
-router.get("/tweets", (req, res) => {
+//Get all tweets from a user
+router.get("/tweets/:userId", (req, res) => {
   userSchema
-    .find()
+    .findById(req.params.userId)
+    .then((data) => {
+      tweetSchema
+        .find({ _id: { $in: data.tweets } })
+        .then((data) => {
+          res.json(data);
+        })
+        .catch((err) => {
+          res.json({ message: err });
+        });
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+//Update tweet from a user
+router.patch("/tweets/:tweetId", (req, res) => {
+  tweetSchema
+    .findByIdAndUpdate(req.params.tweetId, {
+      tweet: req.body.tweet,
+    })
     .then((data) => {
       res.json(data);
     })
@@ -28,34 +62,10 @@ router.get("/tweets", (req, res) => {
     });
 });
 
-//Get tweet by user id
-router.get("/tweets/:id", (req, res) => {
-  userSchema
-    .findById(req.params.user.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json({ message: err });
-    });
-});
-
-//Update tweet text by user id
-router.patch("/tweets/:id", (req, res) => {
-  userSchema
-    .findByIdAndUpdate(req.params.user.id, req.body)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json({ message: err });
-    });
-});
-
-//Delete tweet by user id
-router.delete("/tweets/:id", (req, res) => {
-  userSchema
-    .findByIdAndDelete(req.params.user.id)
+//Delete tweet from a user
+router.delete("/tweets/:tweetId", (req, res) => {
+  tweetSchema
+    .findByIdAndDelete(req.params.tweetId)
     .then((data) => {
       res.json(data);
     })
