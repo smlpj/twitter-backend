9ae30077957a16b5dfc96d9de6c30cf7d35c3eda
smlpj <slopeza@uninorte.edu.co>
2022-11-01 16:05:58 -0500

    Users CRUD done

--- routes/user.js
+++ routes/user.js
@@ -28,4 +28,40 @@ router.get("/users", (req, res) => {
     });
 });
 
+//Get user by id
+router.get("/users/:id", (req, res) => {
+  userSchema
+    .findById(req.params.id)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+//Update user by id
+router.patch("/users/:id", (req, res) => {
+  userSchema
+    .findByIdAndUpdate(req.params.id, req.body)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
+//Delete user by id
+router.delete("/users/:id", (req, res) => {
+  userSchema
+    .findByIdAndDelete(req.params.id)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json({ message: err });
+    });
+});
+
 module.exports = router;
